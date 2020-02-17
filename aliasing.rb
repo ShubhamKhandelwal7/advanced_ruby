ebdc59022f2ce4d1751c832e6f9c578b0de3c0c5
skhandelwal7 <shubham.khandelwal@vinsol.com>
2020-02-17 17:01:56 +0530

    updated naming removed class instance var

--- aliasing.rb
+++ aliasing.rb
@@ -7,33 +7,33 @@ module MyModule
 
   def chained_aliasing(target_meth, alias_val)
     create_aliasing_methods(target_meth, alias_val)
-    alias_methods(target_meth)
-    check_access_specifier(target_meth)
   end
   
   def create_aliasing_methods(target_meth, alias_val)
     target_meth_without_schar = target_meth.to_s.gsub(REGEX_SPECIAL_CHAR, '')
     special_char = target_meth.to_s.match(REGEX_SPECIAL_CHAR)
     special_char = '' if special_char && alias_val[-1] == special_char[1]
-    @with_method = "#{target_meth_without_schar}_with_#{alias_val}#{special_char}"
-    @without_method = "#{target_meth_without_schar}_without_#{alias_val}#{special_char}"
+    with_method_name = "#{target_meth_without_schar}_with_#{alias_val}#{special_char}"
+    without_method_name = "#{target_meth_without_schar}_without_#{alias_val}#{special_char}"
+    alias_methods(target_meth, with_method_name, without_method_name)
   end
 
-  def alias_methods(target_meth)
-    alias_method @without_method, target_meth
-    alias_method target_meth, @with_method
+  def alias_methods(target_meth, with_method_name, without_method_name)
+    alias_method without_method_name, target_meth
+    alias_method target_meth, with_method_name
+    check_access_specifier(target_meth, with_method_name, without_method_name)
   end
 
-  def check_access_specifier(target_meth)
+  def check_access_specifier(target_meth, with_method_name, without_method_name)
     if protected_method_defined?(target_meth)
-      protected @with_method
-      protected @without_method
+      protected with_method_name
+      protected without_method_name
     elsif private_method_defined?(target_meth)
-      private @with_method
-      private @without_method
+      private with_method_name
+      private without_method_name
     else
-      public @with_method
-      public @without_method
+      public with_method_name
+      public without_method_name
     end
   end
 end
@@ -41,11 +41,11 @@ end
 class Hello
   extend MyModule
 
-  def greet
+  private def greet
     puts 'hello'
   end
 
-  def greet_with_logger
+  private def greet_with_logger
     puts '--logging start'
     greet_without_logger
     puts '--logging end'
