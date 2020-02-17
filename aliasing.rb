35c42acdc79a713251c16e6a7b1c4d63d485c636
skhandelwal7 <shubham.khandelwal@vinsol.com>
2020-02-17 12:06:26 +0530

    Printing the output after each line The regex is inside the module Removed define_method for chained_aliasing Avoided the use of $1 Replaced alias with alias_method Added check_access_specifier public method use case

--- aliasing.rb
+++ aliasing.rb
@@ -1,24 +1,27 @@
-REGEX_SPECIAL_CHAR = /(?<k>[\?\=\!])$/
 module MyModule
-  def self.extended(base)
-    define_method('chained_aliasing') do |target_meth, alias_val|
-      create_aliasing_methods(target_meth, alias_val)
-      alias_methods(target_meth)
-      check_access_specifier(target_meth)
-    end
+  REGEX_SPECIAL_CHAR = /(?<k>[\?\=\!])$/
+  
+  def self.included(base)
+    base.extend MyModule
+  end
+
+  def chained_aliasing(target_meth, alias_val)
+    create_aliasing_methods(target_meth, alias_val)
+    alias_methods(target_meth)
+    check_access_specifier(target_meth)
   end
   
   def create_aliasing_methods(target_meth, alias_val)
     target_meth_without_schar = target_meth.to_s.gsub(REGEX_SPECIAL_CHAR, '')
-    special_char = $1
-    special_char = '' if alias_val[-1] == special_char
+    special_char = target_meth.to_s.match(REGEX_SPECIAL_CHAR)
+    special_char = '' if special_char && alias_val[-1] == special_char[1]
     @with_method = "#{target_meth_without_schar}_with_#{alias_val}#{special_char}"
     @without_method = "#{target_meth_without_schar}_without_#{alias_val}#{special_char}"
   end
 
   def alias_methods(target_meth)
-    class_eval("alias #{@without_method} #{target_meth}")
-    class_eval("alias #{target_meth} #{@with_method}")
+    alias_method @without_method, target_meth
+    alias_method target_meth, @with_method
   end
 
   def check_access_specifier(target_meth)
@@ -28,6 +31,9 @@ module MyModule
     elsif private_method_defined?(target_meth)
       private @with_method
       private @without_method
+    else
+      public @with_method
+      public @without_method
     end
   end
 end
@@ -84,22 +90,60 @@ end
 say = Hello.new
 
 say.greet_with_logger
+# ------Output-------
+# --logging start
+# hello
+# --logging end
 say.greet
+# ------Output-------
+# --logging start
+# hello
+# --logging end
 
 p '-------------------------------------'
 
 say.a_with_b?
+# ------Output-------
+# --logging start
+# Inside a? method
+# --logging end
 say.a_without_b?
+# ------Output-------
+# Inside a? method
 say.a?
+# ------Output-------
+# --logging start
+# Inside a? method
+# --logging end
 
 p '-------------------------------------'
 
 say.c_with_d?
+# ------Output-------
+# --logging start
+# Inside c method
+# --logging end
 say.c_without_d?
+# ------Output-------
+# Inside c method
 say.c
+# ------Output-------
+# --logging start
+# Inside c method
+# --logging end
 
 p '-------------------------------------'
 
 say.e_with_f?
+# ------Output-------
+# --logging start
+# Inside e? method
+# --logging end
 say.e_without_f?
+# ------Output-------
+# Inside e? method
 say.e?
+# ------Output-------
+# --logging start
+# Inside e? method
+# --logging end
