a5480527516af8f591b486c25119eb7c719375d7
skhandelwal7 <shubham.khandelwal@vinsol.com>
2020-02-18 19:26:01 +0530

    updated version

--- filters.rb
+++ filters.rb
@@ -35,53 +35,60 @@ module MyModule
   end
 
   def before_methods
-    @before_methods ||= Hash.new { |hash, key| hash[key] = { only: [], except: [] }}
+    @before_methods ||= Hash.new { |hash, key| hash[key] = { only: [], except: [] } }
   end
 
   def after_methods
-    @after_methods ||= Hash.new { |hash, key| hash[key] = { only: [], except: [] }}
+    @after_methods ||= Hash.new { |hash, key| hash[key] = { only: [], except: [] } }
   end
 
   def validate_methods(methods)
     methods.each do |meth|
-      if new.respond_to?(meth, true)
-        raise "#{meth} is not private" unless private_method_defined?(meth)
+      unless meth.is_a?(Proc)
+        if method_defined?(meth, true)
+          raise "#{meth} is not private" unless private_method_defined?(meth)
+        end
       end
     end
   end
 
+  def action_method
+    @action_method ||= []
+  end
+
   def action_methods(*arguments)
-    @action_methods = arguments
-    FilterModule.execute_method(arguments)
+    action_method.concat(arguments)
   end
 
   def method_added(name)
-    if defined?(filter_methods[:before_methods][0][0])
-      validate_methods(filter_methods[:before_methods][0]) unless filter_methods[:before_methods][0].include?(name)
+    if defined?(before_methods) && !defined?(after_methods)
+      validate_methods(Array(name)) unless before_methods.keys.include?(name)
     end
-    if defined?(filter_methods[:after_methods][0][0])
-      validate_methods(filter_methods[:after_methods][0]) unless filter_methods[:after_methods][0].include?(name)
+    if defined?(after_methods) && !defined?(before_methods)
+      validate_methods(Array(name)) unless after_methods.keys.include?(name)
+    end
+    if action_method.include?(name)
+      FilterModule.execute_method(name)
     end
   end
 
   module FilterModule
-    def self.execute_method(action_methods)
-      action_methods.each do |action_meth|
-        create_dynamic_method(action_meth)
-      end
-    end
-
-    def self.create_dynamic_method(action_meth)
+    def self.execute_method(action_meth)
       define_method(action_meth) do
         self.class.before_methods.each_pair do |meth, option|
-          method(meth).call if check_condition(action_meth, option)
+          call_methods(meth, option, action_meth)
         end
         super()
         self.class.after_methods.each_pair do |meth, option|
-          method(meth).call if check_condition(action_meth, option)
+          call_methods(meth, option, action_meth)
         end
       end
-      
+
+      def call_methods(meth, option, action_meth)
+        meth.call if meth.is_a?(Proc) && check_condition(action_meth, option)
+        method(meth).call if !meth.is_a?(Proc) && check_condition(action_meth, option)
+      end
+
       def check_condition(action_meth, option)
         ((option[:only].include?(action_meth) || option[:only].empty?) &&
                                !option[:except].include?(action_meth))
@@ -96,6 +103,7 @@ class MyClass
   action_methods :your_method
 
   before_filter :foo, :bar, only: [:your_method]
+  before_filter proc { puts 'Test Proc' }
   before_filter :baz
   action_methods :my_method
 
@@ -124,7 +132,7 @@ class MyClass
   end
 
   action_methods :our_method
-  after_filter  :maz, except: [:my_method]
+  after_filter :maz, except: [:my_method]
 
   def our_method
     puts 'inside our_method'
@@ -138,6 +146,7 @@ end
 m = MyClass.new
 m.my_method
 # ---Output----
+# Test Proc
 # inside baz
 # inside my_method
 
@@ -147,6 +156,7 @@ m.your_method
 # ---Output----
 # inside foo
 # inside bar
+# Test Proc
 # inside baz
 # inside your_method
 # inside maz
@@ -161,6 +171,7 @@ p '-----------------------'
 
 m.our_method
 # ---Output----
+# Test Proc
 # inside baz
 # inside our_method
 # inside maz
